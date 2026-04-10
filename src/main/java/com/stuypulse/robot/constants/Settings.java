26c35f7c3492dc2c9a43383099d98859d31d3a82
SP-COMPuter <se@stuypulse.com>
2026-04-10 13:39:13 -0400

    Revert "FEAT: doglog spindexer non elastic values and update vendor deps"
    This reverts commit 0b5e2c26c081f970de02bb75c345a288ae5921a8.

--- src/main/java/com/stuypulse/robot/constants/Settings.java
+++ src/main/java/com/stuypulse/robot/constants/Settings.java
@@ -387,7 +387,7 @@ public interface Settings {
         public final double MAX_ANGULAR_VELOCITY_RAD_SEC = 2 * Math.PI;
         double MIN_TAG_AREA = 5; //TODO: MAKE SURE THIS IS A GOOD VALUE!!!
 
-        SmartBoolean HDR_ENABLED = new SmartBoolean("Vision/HDR Enabled?", true);
+        SmartBoolean HDR_ENABLED = new SmartBoolean("Vision/HDR Enabled?", false);
         double HDR_TIMEOUT_SEC = 0.25;
 
         public final double BUZZ_DEBOUNCE = 0.25;
